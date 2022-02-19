6447fd73b962278c09b3df1c14cc422bac56f9bd
jonoman55 <jonoman55@yahoo.com>
2022-02-18 23:44:59 -0500

    TODO added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navbar } from './components';
 import { About, Header, Footer, Skills, Testimonial, Work } from './container';
 import './App.scss';
 
+// TODO : Find better fav icon
 // TODO : Add context API
 const App = () => (
     <div className='app'>
